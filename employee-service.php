030b40636e008b6883f2e3657aca2a9482693f6b
pranesh singha <praneshsingha@rediffmail.com>
2020-02-06 00:27:53 +0530

    navbar and service page created
    service page created with dummy

---
+++ employee-service.php
@@ -0,0 +1,75 @@
+<?php 
+    include('navbar.php');
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/emp-service.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="header-image-section">
+            <img src="images/services.jpg" class="service-image" alt="">
+        </div>
+        <p class="service-title">Our Services</p>
+    </div>
+    <div class="container-fluid">
+        <div class="services-container">
+            
+            <div class="service-img-container">
+                <img src="images/services/service1.jpg" class="service-img" alt="">
+            </div>
+            <div class="service-text-container">
+                <p class="service-sub-title">Service Title-1</p>
+                <p class="service-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum provident suscipit dolor repudiandae deserunt possimus laborum asperiores ipsam ad ut consectetur magni, accusamus modi, dignissimos excepturi iste! Quod molestiae ratione beatae obcaecati, omnis molestias adipisci consequuntur accusantium! Natus, quos.
+                </p>
+            </div>
+
+            <div class="service-text-container">
+                <p class="service-sub-title">Service Title-2</p>
+                <p class="service-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum provident suscipit dolor repudiandae deserunt possimus laborum asperiores ipsam ad ut consectetur magni, accusamus modi, dignissimos excepturi iste! Quod molestiae ratione beatae obcaecati, omnis molestias adipisci consequuntur accusantium! Natus, quos.
+                </p>
+            </div>
+            <div class="service-img-container">
+                <img src="images/services/service2.jpg" class="service-img" alt="">
+            </div>
+
+            <div class="service-img-container">
+                <img src="images/services/service3.jpg" class="service-img" alt="">
+            </div>
+            <div class="service-text-container">
+                <p class="service-sub-title">Service Title-3</p>
+                <p class="service-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum provident suscipit dolor repudiandae deserunt possimus laborum asperiores ipsam ad ut consectetur magni, accusamus modi, dignissimos excepturi iste! Quod molestiae ratione beatae obcaecati, omnis molestias adipisci consequuntur accusantium! Natus, quos.
+                </p>
+            </div>
+
+            <div class="service-text-container">
+                <p class="service-sub-title">Service Title-4</p>
+                <p class="service-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum provident suscipit dolor repudiandae deserunt possimus laborum asperiores ipsam ad ut consectetur magni, accusamus modi, dignissimos excepturi iste! Quod molestiae ratione beatae obcaecati, omnis molestias adipisci consequuntur accusantium! Natus, quos.
+                </p>
+            </div>
+            <div class="service-img-container">
+                <img src="images/services/service4.jpg" class="service-img" alt="">
+            </div>
+
+            <div class="service-img-container">
+                <img src="images/services/service5.jpg" class="service-img" alt="">
+            </div>
+            <div class="service-text-container">
+                <p class="service-sub-title">Service Title-5</p>
+                <p class="service-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum provident suscipit dolor repudiandae deserunt possimus laborum asperiores ipsam ad ut consectetur magni, accusamus modi, dignissimos excepturi iste! Quod molestiae ratione beatae obcaecati, omnis molestias adipisci consequuntur accusantium! Natus, quos.
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="navbar">
+        <span>Empower Salon</span>
+        <ul class="nav-links">
+            <li><a href="">Home</a></li>
+            <li><a href="">Service</a></li>
+            <li><a href="">Link 3</a></li>
+            <li><a href="">Link 4</a></li>
+        </ul>
+    </div>
+</body>
+</html>
