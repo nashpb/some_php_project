d0f8cb025d63799d181a2b0399ca8f716f606af8
mohammedyunnus <60706593+mohammedyunnus@users.noreply.github.com>
2020-02-09 13:49:19 +0530

    phone and email validation

--- registration.php
+++ registration.php
@@ -6,27 +6,40 @@
 	<link rel="stylesheet" href="style/style.css">
 	<script src="script/script.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<style>
+      #emailVal,#phone:invalid {
+        border: 2px solid red;
+      }
+
+      #emailVal,#phone:valid {
+        border: 2px solid green;
+      }
+    </style>
 </head>
 <body class="back-img">
 	<div class="container-fluid display-flex-center login">
 		<div class="login-div">
 			<p class="form-title">Registration</p>
-			<form>
+			<form formname="myForm"  >
 				<div class="input-container">
-					<input type="text" class="form-control" placeholder="Name">
-					<input type="text" class="form-control" placeholder="Username">
-					<input type="text" class="form-control" placeholder="Password">
+					<input type="text" class="form-control" placeholder="Name" required>
+					<input type="text" class="form-control" placeholder="Username" required>
+					<input type="password" id="pass1" name="pswd1"class="form-control" placeholder="Password" minlength="4" maxlength="10"   required >
+
+					<input type="password"  name="pswd2"class="form-control" placeholder="Confirm Password" minlength="4" maxlength="10" required>
 					<input type="text" class="form-control" placeholder="address">
+					<input type="number" class="form-control" placeholder="Phone Number" max="10" id="phone" onfocusout="mobileNumber();">
+					
 				</div>
 				<div class="otp_container">
-					<input type="text" class="form-control" placeholder="Phone Number">
-					<p class="otp-input">Get OTP</p>
+					<input type="email" class="form-control" placeholder="emailid" onfocusout="checkEmail();"id="emailVal" >
+					<button class="otp-input" >Get OTP</button>
 				</div>
 				<div class="input-container" id="otp-container">
-					<input type="text" class="form-control" placeholder="OTP">
+					<input type="text" class="form-control" placeholder="OTP" >
 				</div>
 				<hr>
-				<input type="submit" value="SUBMIT" class="form-submit">
+				<input type="submit" value="SUBMIT" class="form-submit" id="okButton"  >
 			</form>
 		</div>
 	</div>
@@ -38,4 +51,29 @@ $(document).ready(function(){
 		$("#otp-container").css('display','block');
 	})
 });
-</script>
+function checkEmail() {
+        var email = document.getElementById("emailVal");
+        var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!filter.test(email.value)) {
+            alert('Please provide a valid email address');
+            email.focus;
+            return false;
+        }
+    }
+	
+	function mobileNumber(){
+
+var Number = document.getElementById('phone');
+var IndNum = /^[0]?[6789]\d{9}$/;
+
+if(!IndNum.test(Number.value)){
+   alert('please enter valid mobile number');
+   phone.focus;
+   return false;
+}
+	}
+
+
+
+
+</script> 
