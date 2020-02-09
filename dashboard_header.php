2e3811220bd934bce237767d2eed26e708fae36c
praneshsingha <praneshsingha@rediffmail.com>
2020-02-09 20:21:55 +0530

    dashboard pages completed
    all dashboard pages accept payment

---
+++ dashboard_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+    <div class="navbar">
+        <span>Empower Salon</span>
+        <ul class="nav-links">
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Emplpoyee</a></li>
+            <li><a href="">Customer</a></li>
+            <li><a href="">Appoinments</a></li>
+            <li><a href="">Service</a></li>
+        </ul>
+    </div>
+</body>
+</html>
