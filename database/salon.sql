22492d9edc6522728dd9b27e8365ba682fa7d7fd
Nash <nashpb@gmail.com>
2020-03-16 05:39:16 +0530

    New db

--- database/salon.sql
+++ database/salon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 12, 2020 at 01:39 PM
+-- Generation Time: Mar 16, 2020 at 05:37 AM
 -- Server version: 5.7.28-0ubuntu0.18.04.4
 -- PHP Version: 7.2.24-0ubuntu0.18.04.1
 
@@ -63,7 +63,8 @@ CREATE TABLE `appointments` (
 --
 
 INSERT INTO `appointments` (`id`, `cust_id`, `appointment_date`, `appointment_time`, `appointment_service_type`, `appointment_status`, `emp_id`, `created`) VALUES
-(1, 1, '2020-03-10', '1:54 AM', '0', '0', NULL, '2020-03-09 20:25:56');
+(1, 1, '2020-03-10', '1:54 AM', '0', '3', NULL, '2020-03-09 20:25:56'),
+(2, 1, '2020-03-16', '2:56 AM', '1', '2', NULL, '2020-03-15 21:26:04');
 
 -- --------------------------------------------------------
 
@@ -86,7 +87,12 @@ INSERT INTO `appointment_services_junc` (`id`, `appointment_id`, `service_id`) V
 (2, 1, 5),
 (3, 1, 6),
 (4, 1, 8),
-(5, 1, 10);
+(5, 1, 10),
+(6, 2, 8),
+(7, 2, 9),
+(8, 2, 10),
+(9, 2, 11),
+(10, 2, 12);
 
 -- --------------------------------------------------------
 
@@ -99,11 +105,15 @@ CREATE TABLE `customers` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone_no` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `gender` varchar(255) NOT NULL,
-  `verified` varchar(1) NOT NULL,
-  `otp` varchar(6) DEFAULT NULL
+  `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `email`, `phone_no`, `address`) VALUES
+(16, 'N', 'n@m.c', '9731134958', 'lemw;l');
 
 -- --------------------------------------------------------
 
@@ -185,6 +195,30 @@ INSERT INTO `services` (`id`, `name`, `description`, `price`) VALUES
 (12, 'askq', 'dopkqp', '121.00'),
 (13, 'LOL', 'LOL', '20.00');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `user_type` enum('A','E','C') NOT NULL,
+  `user_info_id` int(11) NOT NULL,
+  `verified` enum('0','1') NOT NULL DEFAULT '0',
+  `otp` varchar(6) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_name`, `password`, `user_type`, `user_info_id`, `verified`, `otp`, `created_at`) VALUES
+(1, 'n', 'Qw@1', 'C', 16, '0', NULL, '2020-03-15 23:53:39');
+
 --
 -- Indexes for dumped tables
 --
@@ -215,8 +249,7 @@ ALTER TABLE `appointment_services_junc`
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email_UNIQUE` (`email`),
-  ADD UNIQUE KEY `phone_no_UNIQUE` (`phone_no`),
-  ADD UNIQUE KEY `password_UNIQUE` (`password`);
+  ADD UNIQUE KEY `phone_no_UNIQUE` (`phone_no`);
 
 --
 -- Indexes for table `employees`
@@ -252,6 +285,12 @@ ALTER TABLE `services`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -265,19 +304,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `appointment_services_junc`
 --
 ALTER TABLE `appointment_services_junc`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `employees`
@@ -308,6 +347,12 @@ ALTER TABLE `salon_services_junc`
 --
 ALTER TABLE `services`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
